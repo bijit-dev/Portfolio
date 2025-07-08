3775b0b0b07c4dc00d5c8d5cee205781c7035270
BIJIT DEB <bijitdeb0703@gmail.com>
2025-07-09 00:43:53 +0600

    about section update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,9 +87,18 @@ const Home = () => {
             {/* about section */}
             <div className="container mx-auto px-4 md:px-8 my-20">
                 <h1 className="text-3xl font-bold Poppins text-white text-center mb-10">About Me</h1>
-                <p className="text-lg Roboto font-medium text-white text-center">
-                    I am a web developer with 1+ years of experience in React. I have a strong foundation in front-end & back-end development, skilled in creating user-friendly and responsive web applications using React and its ecosystem.
-                </p>
+                <div className="grid grid-cols-1 lg:grid-cols-2 gap-5">
+                    <div className="text-lg Roboto font-medium text-white text-justify space-y-2">
+                        <p>I’m a passionate and results-driven web developer with specialized expertise in React.js and the MERN stack (MongoDB, Express.js, React, Node.js). I strive to build scalable, maintainable, and high-performance web applications that not only meet client needs but exceed expectations.</p>
+                        <p>What truly sets me apart is my commitment to integrity, clear communication, and client satisfaction. I believe that great development is not just about writing clean code—it's about understanding the vision, solving real-world problems, and delivering value through thoughtful, user-centric design.</p>
+                        <p>Currently, I’m expanding my horizons by diving into emerging web technologies and best practices to stay ahead of the curve. Whether it's improving performance, exploring modern frameworks, or refining development workflows, I’m always learning and evolving.</p>
+                        <p>In every project, I bring a mindset of collaboration, accountability, and continuous improvement—qualities that help turn ideas into impactful digital experiences.</p>
+                    </div>
+
+                    <div>
+
+                    </div>
+                </div>
             </div>
 
             <Footer />
